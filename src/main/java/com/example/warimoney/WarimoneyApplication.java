4585d81e28364b749f710f7be6b34de98e161c0c
SuzukiYutaro <suzuki.yutaro.sy@gmail.com>
2026-05-20 00:14:43 +0900

    first

---
+++ src/main/java/com/example/warimoney/WarimoneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.warimoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarimoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarimoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/warimoney/WarimoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warimoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarimoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
